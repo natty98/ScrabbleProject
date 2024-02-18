4d712bb81a483bfa7bbd6f3089041ee7b2e04c2f
natty98 <ogeh1998@gmail.com>
2024-02-18 17:04:34 -0500

    update to the hashmap of the tiles adding in the values as well

--- src/Finder.java
+++ src/Finder.java
@@ -2,12 +2,40 @@ import java.util.HashMap;
 
 public class Finder {
     /**
-     * This method is used to find the 
+     * This method is used to find the possible words that the user could use and also display their values beside them
      * @param letter - repressents the letter the player wants to play against
      * @param availLetters - represents the letters available to the player(7)
      */
     public static void Find(String letter, String availLetters){
         /*Check the words in the scrabble dictionary that have those words */
         /*Display both the words and their value */
+        HashMap<String,Integer> tile = new HashMap<>();
+
+        tile.put("A",1);
+        tile.put("B",3);
+        tile.put("C",3);
+        tile.put("D",2);
+        tile.put("E",1);
+        tile.put("F",4);
+        tile.put("G",2);
+        tile.put("H",4);
+        tile.put("I",1);
+        tile.put("J",8);
+        tile.put("K",5);
+        tile.put("L",1);
+        tile.put("M",3);
+        tile.put("N",1);
+        tile.put("O",1);
+        tile.put("P",3);
+        tile.put("Q",10);
+        tile.put("R",1);
+        tile.put("S",1);
+        tile.put("T",1);
+        tile.put("U",1);
+        tile.put("V",4);
+        tile.put("W",4);
+        tile.put("X",8);
+        tile.put("Y",4);
+        tile.put("Z",10);
     }
 }
